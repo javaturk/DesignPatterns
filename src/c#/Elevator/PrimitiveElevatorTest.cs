cbafb25eb0293b3c41cb6cb1fb1d11d24eeebe51
Akin Kaldiroglu <akin@javaturk.org>
2021-01-24 18:44:21 +0300

    Add files via upload

---
+++ src/c#/Elevator/PrimitiveElevatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Elevator.domain;
+namespace Elevator {
+    public class PrimitiveElevatorTest {
+		public static void Main(String[] args)
+		{
+			PrimitiveElevator primitiveElevator = new PrimitiveElevator();
+
+			primitiveElevator.OpenDoor();
+			primitiveElevator.CloseDoor();
+			primitiveElevator.GoDown();
+			primitiveElevator.GoUp();
+			primitiveElevator.GoUp();
+			primitiveElevator.OpenDoor();
+			primitiveElevator.CloseDoor();
+			primitiveElevator.GoDown();
+			primitiveElevator.OpenDoor();
+			primitiveElevator.GoDown();
+			primitiveElevator.CloseDoor();
+			primitiveElevator.GoDown();
+			primitiveElevator.GoDown();
+
+			int curFloor = primitiveElevator.GetFloor();
+
+			if (curFloor != 5 && !primitiveElevator.GetDoorStatus())
+			{
+				primitiveElevator.Go(5);
+			}
+
+			primitiveElevator.Go(-10000000);
+			primitiveElevator.OpenDoor();
+		}
+	}
+}
