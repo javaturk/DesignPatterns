c91a3a78608bedc7579f9c8bc1f0b9d7f92e5fb1
Akin Kaldiroglu <akin@javaturk.org>
2021-03-17 17:38:01 +0300

    Add files via upload

---
+++ src/c#/ch01/BasbakanlikKalemi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DesignPatterns.org.javaturk.dp.basic.pattern.gof.structural.proxy.basbakan
+{
+    public class BasbakanlikKalemi
+    {
+        private IBasbakan basbakan;
+
+        public BasbakanlikKalemi(IBasbakan basbakan)
+        {
+            this.basbakan = new VekilBasbakan(basbakan);
+        }
+
+        public IBasbakan BanaBasbakaniVer()
+        {
+            Console.WriteLine("Başbakanlık Kalemi: Tabi efendim");
+            return basbakan;
+        }
+    }
+}
---
+++ src/c#/ch01/Vatandas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace DesignPatterns.org.javaturk.dp.basic.pattern.gof.structural.proxy.basbakan
+{
+    public class Vatandas
+    {
+        private IBasbakan basbakan;
+
+        public Vatandas(BasbakanlikKalemi kalem)
+        {
+            basbakan = kalem.BanaBasbakaniVer();
+        }
+
+        public String DerdiniAnlat()
+        {
+            basbakan.DertDinle("Bir derdim var...");
+            return "Yasasiiinnn!!!";
+        }
+
+
+        public String IsIste()
+        {
+            basbakan.IsBul("Oglum");
+            return "Yasasiiinnn!!!";
+        }
+    }
+}
